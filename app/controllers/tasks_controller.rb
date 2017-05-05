e95788b9cd9faa8696c960bf7692f4d2b19eef57
onoxeve <onoxeve@gmail.com>
2017-05-05 09:08:19 +0000

    add login,change style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,33 +1,24 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def index
-    @tasks = Task.all.page(params[:page])
-  end
-
-  def show
-  end
-
-  def new
-    @task = Task.new
-  end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:succcess] = 'Taskが正常に作成されました'
-      redirect_to @task
+      flash[:success] = 'Taskを作成しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Taskの作成に失敗しました'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'Taskの作成に失敗しました。'
+      render 'toppages/index'
     end
   end
-
+  
   def edit
   end
-
+  
   def update
-
     if @task.update(task_params)
       flash[:succcess] = 'Taskは正常に更新されました'
       redirect_to @task
@@ -36,12 +27,14 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+  
+  def show
+  end
 
   def destroy
     @task.destroy
-    
-    flash[:succcess] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'Taskを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
 
   private
@@ -50,8 +43,14 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
 end
